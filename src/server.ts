b2365dcccea779abfa499141fa2c7ff4f6826ee8
DevOld112 <XwilmerX53@gmail.com>
2024-09-04 21:45:05 -0500

    back

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
 import express from 'express'
-import path from 'path';
 import dotenv from 'dotenv'
 import cors from 'cors'
 import morgan from 'morgan'
@@ -29,14 +28,6 @@ app.use('/api/public', publicRoutes)
 app.use('/api/auth', authRoutes)
 app.use('/api/raffle', raffleRoutes)
 
-// Servir archivos estáticos del frontend
-const distPath = path.join(__dirname, '../dist');
-app.use(express.static(distPath));
-
-// Cualquier otra ruta redirige a index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(distPath, 'index.html'));
-});
 
 
 export default app;
